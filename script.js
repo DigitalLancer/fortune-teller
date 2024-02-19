a30300cd0c29153ee14be70da97ecf6c0f863581
DigitalLancer <62387766+DigitalLancer@users.noreply.github.com>
2024-02-19 13:40:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const orb=document.querySelector(".orb");
+const container=document.querySelector(".container");
+const submitButton=document.querySelector("button");
+const fortuneTeller=document.querySelector("div .fortuneTeller");
+const input=document.querySelector(".textField");
+const speechBubble=document.querySelector(".answer");
+
+const answers = ["Yes", "No", "Most definitely", "Uncertain","Absolutely","Unlikely"];
+
+submitButton.addEventListener('click',()=>{
+   
+    const index=Math.floor(Math.random() * 6);
+    const answer=answers[index];
+    console.log(`Asnwer given: ${answer}`);
+    const input=document.querySelector("#question");
+    question=input.value;
+    input.value="";
+    speechBubble.textContent=answer;
+    speechBubble.style.visibility="visible";
+    
+});
